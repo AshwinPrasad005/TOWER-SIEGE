f7f09b536418f63c489b7280ab23931dc4d8fe2d
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-07-03 14:08:17 +0530

    Add files via upload

--- Shooter.js
+++ Shooter.js
@@ -1,20 +1,24 @@
 class Shooter {
-    constructor(x,y,radius){
+    constructor(x,y,width,height){
         var options ={
             isStatic:false,
             density:0.3,
             friction:1.0
         }
-        this.body = Bodies.circle(x,y,radius,options);
-        this.radius = radius;
+        this.image = loadImage("shooter.png");
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
         World.add(world,this.body);
     }
     display(){
+        // var angle = this.body.angle;
+        var pos =this.body.position;
         push();
-        var pos = this.body.position;
-        ellipseMode(CENTER);
-        fill("blue");
-        ellipse(pos.x,pos.y,this.radius,this.radius);
+        // translate(pos.x,pos.y);
+        // rotate(angle);
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y, this.width, this.height);
         pop();
     }
 }
