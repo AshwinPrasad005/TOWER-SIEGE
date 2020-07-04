b676421b0ca2b3c9bcb1381edf90eb6c0199354f
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-07-04 19:46:49 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -1,9 +1,20 @@
 class Box extends BaseClass {
     constructor(x,y,width,height){
     super(x,y,width,height);
+    this.visibility = 255;
     }
     display(){
         fill("green");
+        if(this.body.speed > 3){
         super.display();
+        }
+        else{
+            World.remove(world, this.body);
+            push();
+            this.Visiblity = this.Visiblity - 5;
+            tint(255,this.Visiblity);
+            rect(this.body.position.x, this.body.position.y,25,25);
+            pop();
+        }
     }
 }
