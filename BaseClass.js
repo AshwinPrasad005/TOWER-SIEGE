b29290601968b305f78ce3060ab9b94c050f0035
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-07-17 08:52:03 +0530

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -18,7 +18,7 @@ class BaseClass{
         translate(pos.x,pos.y);
         rotate(angle);
         imageMode(CENTER);
-        image(this.image,pos.x,pos.y,this.width,this.height);
+        image(this.image,0,0,this.width,this.height);
         pop();
     }
 }
--- box.js
+++ box.js
@@ -13,7 +13,7 @@ class Box extends BaseClass {
             World.remove(world,this.body);
             push();
             this.Visibility = this.Visibility - 5;
-            // tint(255,this.Visibility);
+            tint(255,this.Visibility);
             imageMode(CENTER);
             image(this.image,this.body.position.x, this.body.position.y,25,25);
             pop();
